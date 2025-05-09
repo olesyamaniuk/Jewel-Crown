a1dd2cdad01f64de00eac505f45e3c40196b4933
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-05-09 16:42:37 +0300

    1

---
+++ js/slick.js
@@ -0,0 +1,45 @@
+$(function () {
+  $(".w-n-gallery-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: false,
+    cssEase: "linear",
+    prevArrow: ".w-n-gallery-list-left",
+    nextArrow: ".w-n-gallery-list-right",
+  });
+});
+$(function () {
+  $(".w-n-gallery-lists").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: false,
+    cssEase: "linear",
+    prevArrow: ".w-n-gallery-list-left",
+    nextArrow: ".w-n-gallery-list-right",
+  });
+});
+$(function () {
+  $(".w-n-why-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    cssEase: "linear",
+    prevArrow: ".w-n-why-list-left",
+    nextArrow: ".w-n-why-list-right",
+  });
+});
